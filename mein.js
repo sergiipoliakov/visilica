ff5cfb18ca9fb872c0df56fc28b260a60e65b8fb
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-05-08 00:54:44 +0300

    Add files via upload

---
+++ mein.js
@@ -0,0 +1,25 @@
+//alert("hallo!");
+/*var words = ["программа", "макака", "прекрасный", "оладушек"];
+    var word = words[Math.floor(Math.random() * words.length)];
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) { answerArray[i] = "_"; }
+    var remainingLetters = word.length;
+    while (remainingLetters > 0) { // Основной код 
+        // Показываем состояние игры 
+        // Запрашиваем вариант ответа 
+        // Обновляем answerArray и remainingLetters для каждого 
+        // вхождения угаданной буквы 
+    }
+    alert(answerArray.join(" "));
+    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+    if (guess === null) { break;
+    } else if (guess.length !== 1) { alert("Пожалуйста, введите только одну букву."); } else {
+        // Обновляем состояние игры 
+    }
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            remainingLetters--;
+        }
+
+    }
